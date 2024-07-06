49b2f6bfdb174e6affe7843a067462bcd6899c31
Man LE <lmman724@gmail.com>
2024-07-06 22:46:46 +0700

    Update structure for project

---
+++ music_app/lib/main.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+import 'package:music_app/data/repository/repository.dart';
+import 'package:music_app/ui/home/home.dart';
+
+void main() => runApp(const MusicApp());
