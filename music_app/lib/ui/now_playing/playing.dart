cfad96b425bd4b6297a7199b12e16f2f7740bee0
Man LE <lmman724@gmail.com>
2024-07-11 23:21:26 +0700

    Flutter 11 - Add and rounded songs

--- music_app/lib/ui/now_playing/playing.dart
+++ music_app/lib/ui/now_playing/playing.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 import '../../data/model/song.dart';
 
@@ -26,42 +25,65 @@ class NowPlayingPage extends StatefulWidget {
   State<NowPlayingPage> createState() => _NowPlayingPageState();
 }
 
-class _NowPlayingPageState extends State<NowPlayingPage> {
+class _NowPlayingPageState extends State<NowPlayingPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _imageAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 12000),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return const Scaffold(
-    //   body: Center(
-    //     child: Text("Now Playing"),
-    //   ),
-    // );
+    final screenWidth = MediaQuery.of(context).size.width;
+    const delta = 64;
+    final radius = (screenWidth - delta) / 2;
 
     return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: const Text(
-            "Now Playing",
-          ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_horiz),
-          ),
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text("Now Playing"),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {},
+          child: const Icon(CupertinoIcons.ellipsis),
         ),
-        child: Center(
-          child: Scaffold(
-              body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(widget.playingSong.album),
-                const SizedBox(
-                  height: 16,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(widget.playingSong.album),
+            const SizedBox(height: 16),
+            const Text("_ ___ _"),
+            const SizedBox(height: 48),
+            RotationTransition(
+              turns: Tween(begin: 0.0, end: 1.0)
+                  .animate(_imageAnimationController),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(radius),
+                child: FadeInImage.assetNetwork(
+                  placeholder: "assets/itunes_icon.png",
+                  image: widget.playingSong.image,
+                  width: screenWidth - delta,
+                  height: screenWidth - delta,
+                  imageErrorBuilder: (context, error, stackTrace) {
+                    return Image.asset(
+                      "assets/itunes_icon.png",
+                      width: screenWidth - delta,
+                      height: screenWidth - delta,
+                    );
+                  },
                 ),
-                const Text("_ ___ _"),
-                const SizedBox(
-                  height: 48,
-                )
-              ],
+              ),
             ),
-          )),
-        ));
+          ],
+        ),
+      ),
+    );
   }
 }
