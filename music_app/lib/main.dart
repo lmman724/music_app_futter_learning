cc99cbb6ca4164bf71ee520e536e784aa90c24c4
Man LE <lmman724@gmail.com>
2024-07-11 22:34:29 +0700

    Flutter 9 - Design now playing page

--- music_app/lib/main.dart
+++ music_app/lib/main.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-import 'package:music_app/data/repository/repository.dart';
+// import 'package:music_app/data/repository/repository.dart';
 import 'package:music_app/ui/home/home.dart';
 
 void main() {
