7e3b0e1b18e6f06f0080fd384a43ff88745c5233
Man LE <lmman724@gmail.com>
2024-07-13 21:10:04 +0700

    Futter 10 - Update design album song

--- music_app/lib/ui/now_playing/playing.dart
+++ music_app/lib/ui/now_playing/playing.dart
@@ -60,98 +60,104 @@ class _NowPlayingPageState extends State<NowPlayingPage>
           child: const Icon(CupertinoIcons.ellipsis),
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(widget.playingSong.album),
-            const SizedBox(height: 16),
-            const Text("_ ___ _"),
-            const SizedBox(height: 48),
-            RotationTransition(
-              turns: Tween(begin: 0.0, end: 1.0)
-                  .animate(_imageAnimationController),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(radius),
-                child: FadeInImage.assetNetwork(
-                  placeholder: "assets/itunes_icon.png",
-                  image: widget.playingSong.image,
-                  width: screenWidth - delta,
-                  height: screenWidth - delta,
-                  imageErrorBuilder: (context, error, stackTrace) {
-                    return Image.asset(
-                      "assets/itunes_icon.png",
-                      width: screenWidth - delta,
-                      height: screenWidth - delta,
-                    );
-                  },
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(widget.playingSong.album),
+              const SizedBox(height: 10),
+              const Text("_ ___ _"),
+              const SizedBox(height: 20),
+              RotationTransition(
+                turns: Tween(begin: 0.0, end: 1.0)
+                    .animate(_imageAnimationController),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(radius),
+                  child: FadeInImage.assetNetwork(
+                    placeholder: "assets/itunes_icon.png",
+                    image: widget.playingSong.image,
+                    width: screenWidth - delta,
+                    height: screenWidth - delta,
+                    imageErrorBuilder: (context, error, stackTrace) {
+                      return Image.asset(
+                        "assets/itunes_icon.png",
+                        width: screenWidth - delta,
+                        height: screenWidth - delta,
+                      );
+                    },
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 64, bottom: 16),
-              child: SizedBox(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.share_outlined),
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          widget.playingSong.title,
-                          style:
-                              Theme.of(context).textTheme.bodyMedium!.copyWith(
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium!
-                                        .color,
-                                  ),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          widget.playingSong.artist,
-                          style:
-                              Theme.of(context).textTheme.bodyMedium!.copyWith(
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium!
-                                        .color,
-                                  ),
-                        ),
-                      ],
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.favorite_outline),
-                      color: Theme.of(context).colorScheme.primary,
-                    )
-                  ],
+              Padding(
+                padding: const EdgeInsets.only(top: 64, bottom: 16),
+                child: SizedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.share_outlined),
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            widget.playingSong.title,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .color,
+                                ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            widget.playingSong.artist,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .color,
+                                ),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite_outline),
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 32,
-                left: 24,
-                right: 24,
-                bottom: 16,
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 32,
+                  left: 24,
+                  right: 24,
+                  bottom: 16,
+                ),
+                child: _progressBar(),
               ),
-              child: _progressBar(),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 0,
-                left: 24,
-                right: 24,
-                bottom: 16,
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 0,
+                  left: 24,
+                  right: 24,
+                  bottom: 16,
+                ),
+                child: _mediaButtons(),
               ),
-              child: _mediaButtons(),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
